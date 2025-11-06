bda5e4a26c1066bc4c433000cc712599d06afc5c
fahd <jabbarfahd67@gmail.com>
2025-11-06 01:52:52 +0100

    first commit

---
+++ src/main/java/org/sid/bankaccountservice/BankAccountServiceApplication.java
@@ -0,0 +1,37 @@
+package org.sid.bankaccountservice;
+
+import org.sid.bankaccountservice.entities.BankAccount;
+import org.sid.bankaccountservice.enums.AccountType;
+import org.sid.bankaccountservice.repositories.BankAccountRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.UUID;
+
+@SpringBootApplication
+public class BankAccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountServiceApplication.class, args);
+    }
+    @Bean //le projet va etre cree en demarrage
+    CommandLineRunner start(BankAccountRepository bankAccountRepository) {
+
+        return (args) -> {
+            for (int i =0; i < 10; i++) {
+                BankAccount bankAccount =  BankAccount.builder()
+                        .id(UUID.randomUUID().toString())
+                        .type(Math.random()>0.5? AccountType.CURRENT_ACCOUNT:AccountType.SAVING_ACCOUNT)
+                        .balance(1000+Math.random()+9000)
+                        .createdAt(new Date())
+                        .currency("Mad")
+                        .build();
+                bankAccountRepository.save(bankAccount);
+            }
+        } ;
+    }
+
+}
---
+++ src/main/java/org/sid/bankaccountservice/mappers/AccountMapper.java
@@ -0,0 +1,13 @@
+package org.sid.bankaccountservice.mappers;
+
+import org.sid.bankaccountservice.dto.BankAccountResponseDTO;
+import org.sid.bankaccountservice.entities.BankAccount;
+import org.springframework.beans.BeanUtils;
+
+public class AccountMapper {
+    public BankAccountResponseDTO fromBankAccount(BankAccount bankAccount) {
+        BankAccountResponseDTO bankAccountResponseDTO = new BankAccountResponseDTO();
+        BeanUtils.copyProperties(bankAccount, bankAccountResponseDTO);
+        return bankAccountResponseDTO;
+    }
+}
